d8fc6f079509a31c071017b522c8328c0f4cf8b8
Arnab <contact.arnab16@gmail.com>
2020-12-22 14:39:14 +0530

    Added Everything, don't know why it crashes

--- app/src/main/java/com/example/notesop/MainActivity.kt
+++ app/src/main/java/com/example/notesop/MainActivity.kt
@@ -2,23 +2,46 @@ package com.example.notesop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
 
-    lateinit var viewModel: NoteViewModel
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this, this)
+        recyclerView.adapter = adapter
+
         viewModel = ViewModelProvider(
             this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-        ).get(NoteViewModel::class.java)
-        viewModel.allNotes.observe(this, Observer {
-
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, { list ->
+            list?.let {
+                adapter.updateList(it)
+            }
         })
+
+    }
+
+    override fun onItemClicked(note: Notes) {
+        viewModel.deleteNote(note)
+        Toast.makeText(this, "${note.text} Deleted.", Toast.LENGTH_LONG).show()
+    }
+
+    fun submitData() {
+        val noteText = input.text.toString()
+        if(noteText.isNotEmpty() && noteText.isNotBlank()){
+            viewModel.insertNote(Notes(noteText))
+            Toast.makeText(this, "$noteText added to Notes.", Toast.LENGTH_LONG).show()
+        }
     }
 }
